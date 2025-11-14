7ddfac66189065045e632546199d92ee336ac329
CesarVeras <eduardocvsantos@gmail.com>
2025-11-14 11:57:48 -0300

    Add file to install all my flatpaks

---
+++ flatpak-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+flatpak install app.twintaillauncher.ttl -y
+flatpak install app.zen_browser.zen -y
+flatpak install com.discordapp.Discord -y
+flatpak install com.github.tchx84.Flatseal -y
+flatpak install com.google.Chrome -y
+flatpak install com.ranfdev.DistroShelf -y
+flatpak install com.spotify.Client -y
+flatpak install com.stremio.Stremio -y
+flatpak install com.usebottles.bottles -y
+flatpak install com.visualstudio.code -y
+flatpak install com.vysp3r.ProtonPlus -y
+flatpak install io.github.astralvixen.geforce-infinity -y
+flatpak install io.github.kolunmi.Bazaar -y
+flatpak install io.missioncenter.MissionCenter -y
+flatpak install org.gimp.GIMP -y
+flatpak install org.gnome.Boxes -y
+flatpak install org.gnome.Calculator -y
+flatpak install org.libretro.RetroArch -y
+flatpak install org.onlyoffice.desktopeditors -y
+flatpak install org.qbittorrent.qBittorrent -y
+
+#flatpak install org.freedesktop.Platform.VulkanLayer.gamescope -y
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#flatpak remote-add --if-not-exists --user launcher.moe https://gol.launcher.moe/gol.launcher.moe.flatpakrepo
+#flatpak install org.gnome.Platform//48 -y
+#flatpak install moe.launcher.an-anime-game-launcher -y
+#flatpak install moe.launcher.sleepy-launcher -y
+#flatpak install moe.launcher.the-honkers-railway-launcher -y
